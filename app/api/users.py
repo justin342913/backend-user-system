9c2f6b0187cd1b8fb4275a7f0031294e6665481e
Justin <goodjustinmimi34@gmail.com>
2026-02-08 03:54:34 +0800

    Add POST /users API

--- app/api/users.py
+++ app/api/users.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from typing import List
-from app.schemas.user import UserResponse
+from app.schemas.user import UserResponse ,UserCreate
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -13,3 +13,25 @@ fake_users = [
 @router.get("/", response_model=List[UserResponse])
 def get_users():
     return fake_users
+
+
+@router.post(
+    "/",
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED
+)
+def create_user(user: UserCreate):
+    for u in fake_users:
+        if u["email"] == user.email:
+          raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+
+    new_user = {
+        "id": len(fake_users) + 1,
+        "email": user.email,
+        "is_active": True,
+    }
+    fake_users.append(new_user)
+    return new_user
